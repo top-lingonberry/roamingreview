e30ecbfa4df6c82d48c601de0b833dfeebd46ece
Patrick McHugh <patrick.mchugh4@protonmail.com>
2022-07-27 17:50:04 +0300

    Refactored files. Styles to fix.

--- pages/app.tsx
+++
@@ -1,6 +0,0 @@
-import '..pulic/styles/index.css'
-
-// This default export is required in a new `pages/_app.js` file.
-export default function RoamingReviewApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
